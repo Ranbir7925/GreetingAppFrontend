28d2cc1257353ed79abadcb2e75f2b1234a0d528
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-20 22:48:55 +0530

    add:inner html to show cards and getGreeting function to get all greeting from db

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
+const localhost = `http://localhost:4000/greeting`;
 createGreetingForm = () => {
-    document.getElementById("createGreeting").style.display = "block";
-    document.getElementById("editGreeting").style.display = "none";
-    document.getElementById("deleteGreeting").style.display = "none";
-  };
-  editGreetingForm = () => {
-    document.getElementById("editGreeting").style.display = "block";
-    document.getElementById("createGreeting").style.display = "none";
-    document.getElementById("deleteGreeting").style.display = "none";
-  };
-  deleteGreetingForm = () => {
-    document.getElementById("deleteGreeting").style.display = "block";
-    document.getElementById("editGreeting").style.display = "none";
-    document.getElementById("createGreeting").style.display = "none";
-  };
-  closeForm = () => {
-    document.getElementById("createGreeting").style.display = "none";
-    document.getElementById("editGreeting").style.display = "none";
-    document.getElementById("deleteGreeting").style.display = "none";
-  };
+  document.getElementById("createGreeting").style.display = "block";
+  document.getElementById("postsCards").style.display = "none";
+};
+closeForm = () => {
+  document.getElementById("createGreeting").style.display = "none";
+};
+
+const printCards = (posts) => {
+  let output = "";
+  posts.forEach((post) => {
+    output += `<div class="card" >
+  <div class="box1" name="G1"><a class="greetingBox" onclick="selectWork('${post._id}')">Id:- ${post._id} <br> Name:- ${post.name} <br> 
+  Greeting:- ${post.greeting} <br> Created on:- ${post.createdAt}</a> <br>
+  </div>
+  </div>`;
+  });
+  postsCards.innerHTML = output;
+};
+
+getGreeting = () => {
+  fetch(localhost)
+    .then((response) => response.json())
+    .then(result => printCards(result.data))
+    .catch(err => {
+      return err
+    })
+}
