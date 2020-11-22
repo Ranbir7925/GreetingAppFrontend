dc373e984c7719c1e3c926dcce46589bcc0850f4
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-22 22:26:36 +0530

    add:js for button actions

--- src/app.js
+++ src/app.js
@@ -1,29 +1,6 @@
-const localhost = `http://localhost:4000/greeting`;
-createGreetingForm = () => {
+create = () => {
   document.getElementById("createGreeting").style.display = "block";
-  document.getElementById("postsCards").style.display = "none";
 };
-closeForm = () => {
+closepopup = () => {
   document.getElementById("createGreeting").style.display = "none";
 };
-
-const printCards = (posts) => {
-  let output = "";
-  posts.forEach((post) => {
-    output += `<div class="card" >
-  <div class="box1" name="G1"><a class="greetingBox" onclick="selectWork('${post._id}')">Id:- ${post._id} <br> Name:- ${post.name} <br> 
-  Greeting:- ${post.greeting} <br> Created on:- ${post.createdAt}</a> <br>
-  </div>
-  </div>`;
-  });
-  postsCards.innerHTML = output;
-};
-
-getGreeting = () => {
-  fetch(localhost)
-    .then((response) => response.json())
-    .then(result => printCards(result.data))
-    .catch(err => {
-      return err
-    })
-}
