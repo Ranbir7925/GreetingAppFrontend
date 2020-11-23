ef1a8d78db924d48d2d1837e032661d17662929f
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-23 22:22:49 +0530

    add:Code to implement grids as inner html

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
+
 create = () => {
   document.getElementById("createGreeting").style.display = "block";
 };
 closepopup = () => {
   document.getElementById("createGreeting").style.display = "none";
 };
+
+
+
+
+
+
+const printCards = (posts) => {
+  let output = "";
+  posts.forEach((post) => {
+    output +=`<div class="card-box">
+    <div class="grid-box">
+        <p>Id=</p>
+        <p>Name</p>
+        <p>Mesaage</p>
+        <p>Created on</p>
+    </div>
+</div>`;
+  });
+  getElementById("gridCards").innerHTML = output;
+};
+
