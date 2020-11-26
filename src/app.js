f48253dc7e4fff6d5e3f0e94e5bceabf39ff40ae
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-26 23:32:36 +0530

    add:Regex validation for add new greeting

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const url ='http://localhost:4000/greeting'
+const url = 'http://localhost:4000/greeting'
 let regexValidation = new RegExp(/^[A-Za-z]{3,}$/);
 create = () => {
   document.getElementById("createGreeting").style.display = "block";
@@ -11,13 +11,13 @@ closepopup = () => {
 renderCards = (posts) => {
   let output = "";
   posts.forEach((post) => {
-    output +=`<div class="card-box">
+    output += `<div class="card-box">
     <div class="grid-box">
         <p>Id=${post._id}</p>
         <p>Name=${post.name}</p>
         <p>Mesaage=${post.greeting}</p>
-        <div class='div-card1'><img src="./assets/edit.png"></div>
-        <div class='div-card2'><img src="./assets/delete.png"></div>
+        <div class='div-card1'><img src="./assets/edit.png" onclick="edit(post._id)"></div>
+        <div class='div-card2'><img src="./assets/delete.png" onclick="deleteGreeting(${post._id})"></div>
     </div>
 </div>`;
   });
@@ -25,36 +25,60 @@ renderCards = (posts) => {
 };
 
 
-getGreetings = () =>{
+getGreetings = () => {
   fetch(url)
     .then(res => res.json())
     .then(result => renderCards(result.data))
-    .catch((err) =>{
-      return err
+    .catch((err) => {
+      alert(err)
+      location.reload()
     })
 };
 
-addGreeting = () =>{
+addGreeting = () => {
+
   name = document.getElementById("name").value
-  message = document.getElementById("message").value
+  greeting = document.getElementById("greeting").value
+
+  if (!regexValidation.test(name)) {
+    document.getElementById("invalid-name").style.cssText +=
+      "color: #d02525";
+  }
+  if (!regexValidation.test(greeting)) {
+    document.getElementById("invalid-message").style.cssText +=
+      "color: #d02525";
+  }
+  if (regexValidation.test(name)&&regexValidation.test(name)) {
   let params = {
     method: 'POST',
-    headers: {"Content-type":"application/json"},
+    headers: { "Content-type": "application/json" },
     body: JSON.stringify({
-      name:name,
-      greeting:message
+      name: name,
+      greeting: greeting
     })
   }
-  fetch(url,params)
-  .then(()=>{
-    alert("Data Added Successfully")
-  })
-  .catch((err) => err)
+  fetch(url, params)
+    .then(() => {
+      alert("Data Added Successfully")
+    })
+    .catch(err => alert(err))
   closepopup()
   location.reload();
 };
+}
 
+deleteGreeting = (id) => {
+  let params = {
+    method: 'DELETE'
+  }
+  fetch(`url+${id}`,params)
+  .then(()=>{
+    alert("Greeting deleted Successfully")
+    getGreetings()
+  })
+  .catch(err => alert(err))
+};
 
- test =()=>{
-  console.log("test function");
+edit = (id) =>{
+  console.log("edit function",id);
 }
