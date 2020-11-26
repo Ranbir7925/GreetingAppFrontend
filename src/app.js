bbb2c81cb8cd78f7f1fb49f20c4b5accbd53fb11
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-26 20:26:16 +0530

    feat:Create new greeting function

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const url ='http://localhost:4000/greeting'
+let regexValidation = new RegExp(/^[A-Za-z]{3,}$/);
 create = () => {
   document.getElementById("createGreeting").style.display = "block";
   document.getElementById("gridCards").style.display = "none";
@@ -12,9 +13,11 @@ renderCards = (posts) => {
   posts.forEach((post) => {
     output +=`<div class="card-box">
     <div class="grid-box">
-        <span>Id=${post._id}</span>
-        <span>Name=${post.name}</span>
-        <span>Mesaage=${post.greeting}</span>
+        <p>Id=${post._id}</p>
+        <p>Name=${post.name}</p>
+        <p>Mesaage=${post.greeting}</p>
+        <div class='div-card1'><img src="./assets/edit.png"></div>
+        <div class='div-card2'><img src="./assets/delete.png"></div>
     </div>
 </div>`;
   });
@@ -25,9 +28,33 @@ renderCards = (posts) => {
 getGreetings = () =>{
   fetch(url)
     .then(res => res.json())
-    .then(result => result.data)
-    .then(data => renderCards(data))
+    .then(result => renderCards(result.data))
     .catch((err) =>{
       return err
     })
+};
+
+addGreeting = () =>{
+  name = document.getElementById("name").value
+  message = document.getElementById("message").value
+  let params = {
+    method: 'POST',
+    headers: {"Content-type":"application/json"},
+    body: JSON.stringify({
+      name:name,
+      greeting:message
+    })
+  }
+  fetch(url,params)
+  .then(()=>{
+    alert("Data Added Successfully")
+  })
+  .catch((err) => err)
+  closepopup()
+  location.reload();
+};
+
+
+ test =()=>{
+  console.log("test function");
 }
